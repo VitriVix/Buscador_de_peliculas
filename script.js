db591220557d62db2678182302f11ec89c88a548
Vitrivix <joseluismamanimollisaca@gmail.com>
2023-12-08 00:57:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('searchButton').addEventListener('click', searchMovies);
+
+function searchMovies() {
+    const searchImput = document.getElementById('searchImput').value;
+}
